3dad63360928f81a0fc15c420b30d1f13014a21a
NaN-NaN-sempai <lubizinho-2001@hotmail.com>
2017-10-14 23:57:14 -0300

    update 07

--- js/AnimeBookmarkerJS.js
+++ js/AnimeBookmarkerJS.js
@@ -1,27 +1,100 @@
-var version = "ONLINE! 1.19.75"
+var version = "ONLINE! 1.20.32"
 var defaultBG = "http://hdwallpaper.info/wp-content/uploads/bfi_thumb/cute-anime-girl-wallpaper-hd-n2nvi8veegmrtbd7crlon50iua9xrug2oklh0ntx7k.jpg";
+var optfnc = "on"; var opct = 0;
+var lstfnc = "on"; var lstopct = 0;
 
-document.getElementById('options').style.display = "none";
+document.getElementById('options').style.opacity = 0;
+
 document.getElementById('opensitesbtn').onload = fncBeep();
 
-//OPEN AND CLOSE OPTIONS ######################################################################################################################
+//OPEN AND CLOSE OPTIONS ######################################################################################################################	
+
 function OCoptions(){
-var x = document.getElementById('options');
-    if (x.style.display === 'block') {
-        x.style.display = 'none';
+	if(optfnc === "on"){
+		optfnc = "off";
+		var sitn2 = setInterval(itn, 100);
+
+  function itn() {
+    if (opct >= 1) {
+      clearInterval(sitn2);
+	  opct = 1;
+
     } else {
-        x.style.display = 'block';
+      opct = opct + 0.1;
+  document.getElementById('options').style.opacity = opct;
     }
+  }
+  
+  
+  
+} else {
+	optfnc = "on";
+		var sitn2 = setInterval(itn2, 100);
+		
+  function itn2() {
+    if (opct <= 0) {
+      clearInterval(sitn2);
+	  opct = 0;
+
+	  } else {
+      opct = opct - 0.1;
+  document.getElementById('options').style.opacity = opct;
+    }
+  }
+}
 }
 
 function openList(){
-var x = document.getElementById('animeList');
+	if(document.getElementById('clList').innerHTML === 'Open list'){
+		document.getElementById('clList').innerHTML = 'Close list';
+		}else{
+		document.getElementById('clList').innerHTML = 'Open list';
+	}
+	
+	if(lstfnc === "on"){
+	lstfnc = "off";
+		var sitn2 = setInterval(itn2, 100);
+
+  function itn2() {
+    if (lstopct >= 1) {
+      clearInterval(sitn2);
+	  lstopct = 1;
+
+    } else {
+      lstopct = lstopct + 0.1;
+  document.getElementById('animeList').style.opacity = lstopct;
+    }
+  }
+  
+  
+  
+} else {
+	lstfnc = "on";
+		var sitn2 = setInterval(itn3, 100);
+		
+  function itn3() {
+    if (lstopct <= 0) {
+      clearInterval(sitn2);
+	  lstopct = 0;
+
+	  } else {
+      lstopct = lstopct - 0.1;
+  document.getElementById('animeList').style.opacity = lstopct;
+    }
+  }
+}
+}
+	
+	
+	
+	
+/*var x = document.getElementById('animeList');
     if (x.style.display === 'block') {
         x.style.display = 'none';
     } else {
         x.style.display = 'block';
-    }
-}
+    }*/
+
 
 //CHANGE BACKGROUND  #########################################################################################################################
 function mudarBGfnc(){
@@ -84,7 +157,7 @@ document.title = "Animes BookMarker";
 
 function animeListFnc(){
 	setTimeout( function(){
-document.getElementById('animeList').style.display = "none";
+document.getElementById('animeList').style.opacity = 0;
 	}, 3000);
 }
 
